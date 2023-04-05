493def712b8c770ef340a0952826e2976831cd66
Divu <d1403kr3@gmail.com>
2023-04-05 17:09:42 +0530

    StudentDetails

---
+++ src/StudentDetails.Application.Contracts/Students/CreateUpdateStudentDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StudentDetails.Students
+{
+    public class CreateUpdateStudentDto
+    {
+        [Required (ErrorMessage ="Not more than 50 letters")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        public int Roll { get; set; }
+
+        [Required(ErrorMessage = "Must be an unique email address")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Must be a phone number")]
+        [DataType(DataType.PhoneNumber)]
+        public long Phone { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
---
+++ src/StudentDetails.Application/Students/StudentAppService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace StudentDetails.Students;
+
+[Authorize]
+public class StudentAppService :
+    CrudAppService<
+        Student, 
+        StudentDto, 
+        Guid, 
+        PagedAndSortedResultRequestDto, 
+        CreateUpdateStudentDto>,
+    IStudentAppService 
+{
+    public StudentAppService(IRepository<Student, Guid> repository)
+        : base(repository)
+    {
+
+    }
+}
